7bc83702be0c48b5c2862810ef803cb9039a80f8
Sophia Bartsch <s.bartsch@iks-gmbh.com>
2024-09-17 15:50:50 +0200

    get robots from external API

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) => {
