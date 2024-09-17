a05cb64c719a1405e3d7e7b7c6d4d29aab73d649
Sophia Bartsch <s.bartsch@iks-gmbh.com>
2024-09-17 13:24:07 +0200

    + search functionality

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
-const App = () => {
-    return (
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+
+        return (
         <div className="tc">
             <h1>RoboFriends</h1>
-            <SearchBox/>
-            <CardList robots={robots}/>
+            <SearchBox onSearchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots}/>
         </div>
-    );
+        );
+    }
 };
 
 export default App;
